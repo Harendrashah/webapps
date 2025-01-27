0a06d525d13beaa03a8ee0e6e28b464e4f4a0b66
Harendra Kumar Shah <sahh88645@gmail.com>
2025-01-27 21:39:40 +0545

    first sds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt'],
 
   app: {
-    baseURL: '/webapps/', // Base URL for GitHub Pages
+    baseURL: '/webapps/', // Adjust for GitHub Pages
   },
 
   css: [
     '~/assets/css/tailwind.css', // Include Tailwind CSS
   ],
 
-  tailwindcss: {
-    viewer: false, // Disable Tailwind viewer in production
-  },
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-
   target: 'static',
 
   generate: {
-    fallback: '404.html', // Fallback for SPA routing
+    fallback: '404.html', // SPA routing fallback
   },
+
+  compatibilityDate: '2025-01-27',
 });
